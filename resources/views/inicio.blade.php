e139f3ede53bdf1027e99b2e9593072c151c3c17
Scarlhet lopez <lopez.scarlhet@gmail.com>
2017-10-12 12:01:26 -0400

    intercom

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -306,5 +306,12 @@
     </div>
     <a href="#" target="_blank" id="view-source" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-color--accent mdl-color-text--accent-contrast">Productos y servicios</a>
     <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <script>
+      window.intercomSettings = {
+        app_id: "dyvnoakz"
+      };
+  </script>
+  <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/dyvnoakz';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+
   </body>
 </html>
