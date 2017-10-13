90373dccb50cd3227618bc2cf90f9f0fb981b32c
Scarlhet lopez <lopez.scarlhet@gmail.com>
2017-10-13 15:18:23 -0400

    servicios

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -61,7 +61,7 @@
           <div class="android-navigation-container" style="/*background: #A028E6;*/">
             <nav class="android-navigation mdl-navigation">
               <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Inicio</a>
-              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Productos/servicios</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="#servicios">Productos/servicios</a>
               <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Quienes somos</a>
               <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Testimonios</a>
               <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Contactos</a>
@@ -116,14 +116,14 @@
       <div class="android-content mdl-layout__content">
         <a name="top"></a>
         <div class="android-be-together-section mdl-typography--text-center">
-          <div class="logo-font android-slogan">be together. not the same.</div>
+          <div class="logo-font android-slogan">Feel free to look around</div>
           <div class="logo-font android-sub-slogan">welcome to u4web... be yourself. do your thing. see what's going on.</div>
           <!--div class="logo-font android-create-character">
             <a href=""><img src="images/andy.png"> create your android character</a>
           </div-->
-          <div style="padding-top: 250px;">
+          <!--div style="padding-top: 250px;">
 			<a href="https:&#x2F;&#x2F;payment.payphonetodoesposible.com&#x2F;/Pay/1000/6190477d-a58c-4a97-86ce-73cfe61d5962/es/" class="payphone" onmouseover="this.style.backgroundColor='#ff8400'" onmouseout="this.style.backgroundColor='#ff9100'">Pagar $5 con PayPhone</a>
-		  </div>
+		  </div-->
 
           <a href="#screens">
             <button class="android-fab mdl-button mdl-button--colored mdl-js-button mdl-button--fab mdl-js-ripple-effect">
@@ -196,79 +196,53 @@
           <div class="android-customized-section-image"></div>
         </div>
         <div class="android-more-section">
-          <div class="android-section-title mdl-typography--display-1-color-contrast">More from Android</div>
+          <div class="android-section-title mdl-typography--display-1-color-contrast" id="servicios">Marketing digital</div>
           <div class="android-card-container mdl-grid">
-            <div class="mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+            <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
               <div class="mdl-card__media">
                 <img src="images/more-from-1.png">
               </div>
               <div class="mdl-card__title">
-                 <h4 class="mdl-card__title-text">Get going on Android</h4>
+                 <h4 class="mdl-card__title-text">Servicio 1</h4>
               </div>
               <div class="mdl-card__supporting-text">
                 <span class="mdl-typography--font-light mdl-typography--subhead">Four tips to make your switch to Android quick and easy</span>
               </div>
               <div class="mdl-card__actions">
-                 <a class="android-link mdl-button mdl-js-button mdl-typography--text-uppercase" href="">
-                   Make the switch
-                   <i class="material-icons">chevron_right</i>
-                 </a>
+                 <a href="https:&#x2F;&#x2F;payment.payphonetodoesposible.com&#x2F;/Pay/1000/6190477d-a58c-4a97-86ce-73cfe61d5962/es/" class="payphone" onmouseover="this.style.backgroundColor='#ff8400'" onmouseout="this.style.backgroundColor='#ff9100'">Pagar $5 con PayPhone</a>
               </div>
             </div>
 
-            <div class="mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+            <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
               <div class="mdl-card__media">
                 <img src="images/more-from-4.png">
               </div>
               <div class="mdl-card__title">
-                 <h4 class="mdl-card__title-text">Create your own Android character</h4>
+                 <h4 class="mdl-card__title-text">Servicio 2</h4>
               </div>
               <div class="mdl-card__supporting-text">
                 <span class="mdl-typography--font-light mdl-typography--subhead">Turn the little green Android mascot into you, your friends, anyone!</span>
               </div>
               <div class="mdl-card__actions">
-                 <a class="android-link mdl-button mdl-js-button mdl-typography--text-uppercase" href="">
-                   androidify.com
-                   <i class="material-icons">chevron_right</i>
-                 </a>
+                 <a href="https:&#x2F;&#x2F;payment.payphonetodoesposible.com&#x2F;/Pay/1000/6190477d-a58c-4a97-86ce-73cfe61d5962/es/" class="payphone" onmouseover="this.style.backgroundColor='#ff8400'" onmouseout="this.style.backgroundColor='#ff9100'">Pagar $10 con PayPhone</a>
               </div>
             </div>
 
-            <div class="mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+            <div class="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
               <div class="mdl-card__media">
                 <img src="images/more-from-2.png">
               </div>
               <div class="mdl-card__title">
-                 <h4 class="mdl-card__title-text">Get a clean customisable home screen</h4>
+                 <h4 class="mdl-card__title-text">Servicio 3</h4>
               </div>
               <div class="mdl-card__supporting-text">
                 <span class="mdl-typography--font-light mdl-typography--subhead">A clean, simple, customisable home screen that comes with the power of Google Now: Traffic alerts, weather and much more, just a swipe away.</span>
               </div>
               <div class="mdl-card__actions">
-                 <a class="android-link mdl-button mdl-js-button mdl-typography--text-uppercase" href="">
-                   Download now
-                   <i class="material-icons">chevron_right</i>
-                 </a>
+                <a href="https:&#x2F;&#x2F;payment.payphonetodoesposible.com&#x2F;/Pay/1000/6190477d-a58c-4a97-86ce-73cfe61d5962/es/" class="payphone" onmouseover="this.style.backgroundColor='#ff8400'" onmouseout="this.style.backgroundColor='#ff9100'">Pagar $15 con PayPhone</a>
               </div>
             </div>
-
-            <div class="mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
-              <div class="mdl-card__media">
-                <img src="images/more-from-3.png">
-              </div>
-              <div class="mdl-card__title">
-                 <h4 class="mdl-card__title-text">Millions to choose from</h4>
-              </div>
-              <div class="mdl-card__supporting-text">
-                <span class="mdl-typography--font-light mdl-typography--subhead">Hail a taxi, find a recipe, run through a temple – Google Play has all the apps and games that let you make your Android device uniquely yours.</span>
-              </div>
-              <div class="mdl-card__actions">
-                 <a class="android-link mdl-button mdl-js-button mdl-typography--text-uppercase" href="">
-                   Find apps
-                   <i class="material-icons">chevron_right</i>
-                 </a>
-              </div>
-            </div>
+            <br>
           </div>
         </div>
 
@@ -322,7 +296,7 @@
         </footer>
       </div>
     </div>
-    <a href="#" target="_blank" id="view-source" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-color--accent mdl-color-text--accent-contrast">Productos y servicios</a>
+    <a href="#servicios" id="view-source" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-color--accent mdl-color-text--accent-contrast">Productos y servicios</a>
     <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <script>
       window.intercomSettings = {
