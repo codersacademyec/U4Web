6ef00ce3a742e95614b0d2ceb02c83a8dbd196a9
Scarlhet lopez <lopez.scarlhet@gmail.com>
2017-10-24 16:01:30 -0400

    planes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,10 @@
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link rel="stylesheet" href="css/colorbox.css" type="text/css">
+  <link rel="stylesheet" href="css/animate.css" type="text/css">
+  <link href="js/owl-carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="js/owl-carousel/assets/owl.theme.default.min.css" rel="stylesheet">
 </head>
 <body>
   <nav class="transparent" role="navigation" id="top">
@@ -37,7 +41,7 @@
     <div class="section no-pad-bot">
       <div class="container">
         <br><br>
-        <h1 class="header center text-lighten-2 mov ">BIENVENIDOS</h1>
+        <h1 class="header center text-lighten-2 mov">BIENVENIDOS</h1>
         <div class="row center">
           <!--h5 class="header col s12 light">U4Innovation</h5-->
         </div>
@@ -139,19 +143,17 @@
               <div class="col s12 m4">
                 <div class="card blue">
                   <div class="card-content white-text">
-                    <span class="card-title center">Pymes 1.0</span>
+                    <span class="card-title center">Express</span>
                     <hr class="line">
                     <h5>Gestion de dos redes sociales</h5>
-                    <p> 20 post al mes</p>
-                    <p>Diseño - contenidos</p>
-                    <h5>Paquetes de clics</h5>
-                    <p> 1000 clics ads</p>
-                    <h5>Informe de rendimiento</h5>
-                    <p>Quincenal</p>
+                    <p> 5 posteos en cada red social semanal (artes, reposteos, noticias en referencia al giro 
+                      del negocio)</p>
+                    <h5>Diseñador digital</h5>
+                    <h5>Informe de rendimiento mensual</h5>
                     
                   </div>
                   <div class="card-action">
-                  <a href="https:&#x2F;&#x2F;payment.payphonetodoesposible.com&#x2F;/Pay/35000/6190477d-a58c-4a97-86ce-73cfe61d5962/es/" class="waves-effect waves-light btn fbutton" onmouseover="this.style.backgroundColor='#8bc34a'" onmouseout="this.style.backgroundColor='#4CAF50'">Pagar $350 usd + iva con PayPhone</a>
+                  <a href="https:&#x2F;&#x2F;payment.payphonetodoesposible.com&#x2F;/Pay/20000/6190477d-a58c-4a97-86ce-73cfe61d5962/es/" class="waves-effect waves-light btn fbutton" onmouseover="this.style.backgroundColor='#8bc34a'" onmouseout="this.style.backgroundColor='#4CAF50'">Pagar $200 usd + iva con PayPhone</a>
                  </div>
               </div>
                </div>
@@ -159,24 +161,22 @@
                <div class="col s12 m4">
                 <div class="card teal">
                   <div class="card-content white-text">
-                    <span class="card-title center">Pymes 2.0</span>
+                    <span class="card-title center">Inicial</span>
                     <hr class="line">
                     <h5>Community Manager</h5>
                     <p> Interaccion con seguidores </p>
                     <p> Respuestas a consultas básicas de los usuarios</p>
                     <h5>Gestión de dos redes sociales</h5>
-                    <p> 20 post al mes</p>
-                    <p>Diseño - contenidos</p>
-                    <p>12 fotos mensuales</p>
-                    <h5>Paquete de clics</h5>
-                    <p>1000 clics ads</p>
-                    <h5>Video promocional de 30 segundos</h5>
-                     <p>1 video promocional de marca</p>
-                     <h5>Informe de rendimiento</h5>
-                    <p>Quincenal</p>
+                    <p> 5 posteos en cada red social semanal (artes, reposteos, noticias en referencia al giro 
+                      del negocio)</p>
+                    <h5>Diseñador digital</h5>
+                    <p>3 diseños básicos de cortesía semanales (artes)</p>
+                    <h5>Un video promocional de 30 a 45 seg</h5>
+                    <p>(Fotografías otorgadas por el cliente)</p>
+                    <h5>Informe de rendimiento mensual</h5>
                   </div>
                   <div class="card-action">
-                     <a href="https:&#x2F;&#x2F;payment.payphonetodoesposible.com&#x2F;/Pay/55000/6190477d-a58c-4a97-86ce-73cfe61d5962/es/" class="waves-effect waves-light btn fbutton" onmouseover="this.style.backgroundColor='#8bc34a'" onmouseout="this.style.backgroundColor='#4CAF50'">Pagar $550 usd + iva con PayPhone</a>
+                     <a href="https:&#x2F;&#x2F;payment.payphonetodoesposible.com&#x2F;/Pay/40000/6190477d-a58c-4a97-86ce-73cfe61d5962/es/" class="waves-effect waves-light btn fbutton" onmouseover="this.style.backgroundColor='#8bc34a'" onmouseout="this.style.backgroundColor='#4CAF50'">Pagar $400 usd + iva con PayPhone</a>
                  </div>
               </div>
                </div>
@@ -185,28 +185,28 @@
                <div class="col s12 m4">
                 <div class="card green">
                   <div class="card-content white-text">
-                    <span class="card-title center">Pymes 3.0</span>
+                    <span class="card-title center">Empresarial</span>
                     <hr class="line">
                     <h5>Community Manager</h5>
                     <p> Interaccion con seguidores </p>
                     <p> Respuestas a consultas básicas de los usuarios</p>
-                    <h5>Gestión de dos redes sociales</h5>
-                    <p> 24 post al mes</p>
-                    <p>Diseño - contenidos</p>
-                    <p>15 fotos mensuales</p>
-                    <h5>Paquete de clics</h5>
-                    <p>2000 clics ads</p>
-                    <h5>Email Mrketing</h5>
-                    <p>Quincenal</p>
-                    <h5>Base de Datos</h5>
-                    <p>Propia de la empresa en base a fans</p>
-                    <h5>Video promocional de 30 segundos</h5>
-                    <p>1 video promocional de marca</p>
-                    <h5>Informe de rendimiento</h5>
-                    <p>Quincenal</p>
+                    <h5>Gestión de tres redes sociales</h5>
+                    <p> 8 posteos en cada red social semanal. Diseño y Plan semanal de estrategia a seguir con los posteos
+                      junto con el cliente.
+                    </p>
+                    <h5>Diseñador digital</h5>
+                    <p>Diseños básicos de cortesía</p>
+                    <h5>Un video promocional de 30 a 45 seg</h5>
+                    <p>(Fotografías realizadas por nosotros)</p>
+                    <h5>Creacion ca campaña para envio de e-mail marketing a base de 1000 clientes</h5>
+                    <p>(Cliente debe proporcionar el concepto de lo que desea promocionar)</p>
+                    <h5>Informe de rendimiento mensual</h5>
+                    <p>Dos videos promocionales de 30 a 45 seg. (Fotografias realizadas por nosotros)
+                      Informe de estadisticas por cada red social.
+                    </p>
                   </div>
                   <div class="card-action">
-                     <a href="https:&#x2F;&#x2F;payment.payphonetodoesposible.com&#x2F;/Pay/75000/6190477d-a58c-4a97-86ce-73cfe61d5962/es/" class="waves-effect waves-light btn" onmouseover="this.style.backgroundColor='#8bc34a'" onmouseout="this.style.backgroundColor='#8bc34a'">Pagar $700 usd + iva con PayPhone</a>
+                     <a href="https:&#x2F;&#x2F;payment.payphonetodoesposible.com&#x2F;/Pay/60000/6190477d-a58c-4a97-86ce-73cfe61d5962/es/" class="waves-effect waves-light btn" onmouseover="this.style.backgroundColor='#8bc34a'" onmouseout="this.style.backgroundColor='#8bc34a'">Pagar $600 usd + iva con PayPhone</a>
                  </div>
               </div>
             </div>
@@ -220,7 +220,35 @@
       <div class="section">
         <div class="row">
           <div class="col s12 center" id="portafolio">  
+
             <h3 class="strong">Portafolio</h3>
+
+            <div class="col s12 m12 l12">
+               <div class="slide-portafolio owl-carousel owl-theme">
+
+                    <img rel="img_portafolio" style="max-width:350px !important" src="images/redes_sociales.png" alt="">
+
+                    <img rel="img_portafolio" style="max-width:350px !important" src="images/paginas-web.png" alt="">
+
+                    <img rel="img_portafolio" style="max-width:350px !important" src="images/aplicaciones.jpg" alt="">
+
+                    <img rel="img_portafolio" style="max-width:350px !important" src="images/email_mkt.png" alt="">
+
+                    <img rel="img_portafolio" style="max-width:350px !important" src="images/ERPCRM.jpg" alt="">
+
+                    <img rel="img_portafolio" style="max-width:350px !important" src="images/branding.jpg" alt="">
+            </div>
+         </div>
+
+            <div class="row center">
+                <a href="#modal1" class="waves-effect waves-light btn modal-trigger fbutton" onmouseover="this.style.backgroundColor='#8bc34a'" onmouseout="" style="border-radious: 25px;" >MÁS TRABAJOS</a>
+           </div>
+    
+
+          <div class="col s12 m12 l12">
+            <p class="grey-text services">REDES SOCIALES - PÁGINAS WEB - APLICACIONES - EMAIL MKT - CRM & ERP - BRANDING </p>
+          </div>
+
               <div class="col l3 m3 s6" style="border-style: solid; border-width: 0.05px;">
                 <img src="images/logo-asi.png" alt="portafolio" class="responsive-img">
               </div>  
@@ -296,7 +324,36 @@
     </div>
   </div>
   </div>
-  
+
+     <!-- Modal Structure -->
+    <div id="modal1" class="modal bottom-sheet">
+      <div class="modal-content">
+        <h4>Titulo</h4>
+        <p>Cuerpo</p>
+        <div class="row">
+        <div class="col l12 s12">
+          <div class="col s6 m3 l3">
+            <img class="materialboxed responsive-img" data-caption="A picture of a way with a group of trees in a park"  src="images/background1.jpg">
+          </div>
+          <div class="col s6 m3 l3">
+            <img class="materialboxed responsive-img" data-caption="A picture of a way with a group of trees in a park"  src="images/background1.jpg">
+          </div>
+           <div class="col s6 m3 l3">
+            <img class="materialboxed responsive-img" data-caption="A picture of a way with a group of trees in a park"  src="images/background1.jpg">
+          </div>
+          <div class="col s6 m3 l3">
+            <img class="materialboxed responsive-img" data-caption="A picture of a way with a group of trees in a park"  src="images/background1.jpg">   
+          </div>
+          
+        </div>
+      </div>
+     </div>
+
+      <div class="modal-footer">
+        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Cerrar</a>
+      </div>
+    </div>
+           
 
   <footer class="page-footer teal" id="footer">
     <div class="container center">
@@ -350,8 +407,89 @@
       window.intercomSettings = {
         app_id: "dyvnoakz"
       };
+
+        $(document).ready(function(){
+          $('.modal').modal();
+       });
+
+      $('.carousel.carousel-slider').carousel({fullWidth: true});
   </script>
+  <script>
+            $(document).ready(function(){
+            $("filterTwo").click(function(){
+                $("ver_estado").toggle(1000, function(){
+                    alert("The toggle() method is finished!");
+                });
+            });
+
+            $('.slide-portafolio').owlCarousel({
+                // loop: true,
+                // center: true,
+                autoWidth:true,
+                // autoHeight:true,
+                // animateOut: 'slideOutDown',
+                // animateIn: 'flipInX',
+                //autoplay: true,
+                items:1,
+                margin:10,
+                stagePadding:30,
+                smartSpeed:450,
+                // lazyLoad:true
+            });
+
+            $(".img-slider").colorbox({
+                rel:'img-slider',
+                current: "Imagen {current} de {total}",
+                height:"80%"
+            });
+
+
+        });
+
+
+        function resizeImage(selector) {
+            // Remove the previous values
+            // $(selector).css("width", "");
+            $(selector).css("height", "550");
+        
+            $(selector).each(function () {
+                // var maxWidth = 800; // Max width for the image
+                var maxHeight = 550;    // Max height for the image
+                var ratio = 0;  // Used for aspect ratio
+                var width = $(this).width();    // Current image width
+                var height = $(this).height();  // Current image height
+
+                if(height > width){
+                    $(this).css("width", "40%");
+                }
+        
+                // Check if current height is larger than max
+                // if (height > maxHeight) {
+                //     ratio = maxHeight / height; // get ratio for scaling image
+                //     $(this).css("height", maxHeight);   // Set new height
+                //     $(this).css("width", width * ratio);    // Scale width based on ratio
+                //     width = width * ratio;    // Reset width to match scaled image
+                // }
+                // Check if the current width is larger than the max
+                // else if (width > maxWidth) {
+                //     ratio = maxWidth / width;   // get ratio for scaling image
+                //     $(this).css("width", maxWidth); // Set new width
+                //     // $(this).css("height", height * ratio);  // Scale height based on ratio
+                //     height = height * ratio;    // Reset height to match scaled image
+                //     width = width * ratio;    // Reset width to match scaled image
+                // }
+            });
+        
+            // setting up the styles
+            $(selector).css("margin-left", "auto");
+            $(selector).css("margin-right", "auto");
+            $(selector).css("display", "block");
+            $(selector).parent().css("background-image", "none");
+        }
+    </script>
   <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/dyvnoakz';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+  <script type="text/javascript" src="js/owl-carousel/owl.carousel.min.js"></script>
+  <script type="text/javascript" src="js/jquery.colorbox-min.js"></script>
 
 
   </body>
